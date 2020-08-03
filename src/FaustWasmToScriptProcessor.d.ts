4d720921e23219f58c7b9a5324ae9ccf1e42d9f7
Dominique Fober <fober@grame.fr>
2020-08-03 09:49:58 +0200

    remove declaration files

--- src/FaustWasmToScriptProcessor.d.ts
+++
@@ -1,8 +0,0 @@
-import { Faust } from "./Faust";
-import { FaustScriptProcessorNode, TAudioNodeOptions } from "./types";
-export declare class FaustWasmToScriptProcessor {
-    faust: Faust;
-    constructor(faust: Faust);
-    private initNode;
-    getNode(optionsIn: TAudioNodeOptions): Promise<FaustScriptProcessorNode>;
-}
--- src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { Faust } from "./Faust";
-export { FaustAudioWorkletNode } from "./FaustAudioWorkletNode";
